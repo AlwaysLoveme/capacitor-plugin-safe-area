43e76ec257c6851394af71a76048dc6a5d413978
codingman <18270244870@163.com>
2022-09-01 13:50:45 +0800

    fix: deprecated displayCutout to get SafeArea info on android

--- android/src/main/java/com/zhuxian/safearea/SafeArea.java
+++ android/src/main/java/com/zhuxian/safearea/SafeArea.java
@@ -14,27 +14,25 @@ public class SafeArea {
             Log.i(SafeAreaPlugin.class.toString(), String.format("Requires at least %d+", Build.VERSION_CODES.P));
             return this.result(0, 0, 0, 0);
         }
-
         WindowInsets windowInsets = bridge.getActivity().getWindow().getDecorView().getRootWindowInsets();
         if (windowInsets == null) {
             Log.i(SafeAreaPlugin.class.toString(), "WindowInsets is not available.");
             return this.result(0, 0, 0, 0);
         }
+        //        DisplayCutout displayCutout = windowInsets.getDisplayCutout();
+        //        if (displayCutout == null) {
+        //            Log.i(SafeAreaPlugin.class.toString(), "DisplayCutout is not available.");
+        //            int top = Math.round(windowInsets.getStableInsetTop() / density);
+        //            int left = Math.round(windowInsets.getStableInsetLeft() / density);
+        //            int right = Math.round(windowInsets.getStableInsetRight() / density);
+        //            int bottom = Math.round(windowInsets.getStableInsetBottom() / density);
+        //            return this.result(top, left, right, bottom);
+        //        }
         float density = this.getDensity(bridge);
-
-        DisplayCutout displayCutout = windowInsets.getDisplayCutout();
-        if (displayCutout == null) {
-            Log.i(SafeAreaPlugin.class.toString(), "DisplayCutout is not available.");
-            int top = Math.round(windowInsets.getStableInsetTop() / density);
-            int left = Math.round(windowInsets.getStableInsetLeft() / density);
-            int right = Math.round(windowInsets.getStableInsetRight() / density);
-            int bottom = Math.round(windowInsets.getStableInsetBottom() / density);
-            return this.result(top, left, right, bottom);
-        }
-        int top = Math.round(displayCutout.getSafeInsetTop() / density);
-        int left = Math.round(displayCutout.getSafeInsetLeft() / density);
-        int right = Math.round(displayCutout.getSafeInsetRight() / density);
-        int bottom = Math.round(displayCutout.getSafeInsetBottom() / density);
+        int top = Math.round(windowInsets.getStableInsetTop() / density);
+        int left = Math.round(windowInsets.getStableInsetLeft() / density);
+        int right = Math.round(windowInsets.getStableInsetRight() / density);
+        int bottom = Math.round(windowInsets.getStableInsetBottom() / density);
 
         return this.result(top, left, right, bottom);
     }
