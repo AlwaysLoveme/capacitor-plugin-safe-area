60adbed3a7736406cda470f959c6e68c31f0dffb
zhuxian <18270244870@163.com>
2024-04-17 15:08:33 +0800

    fix: setImmersiveNavigationBar not worked on some android device

--- android/src/main/java/com/capacitor/safearea/SafeAreaPlugin.java
+++ android/src/main/java/com/capacitor/safearea/SafeAreaPlugin.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.OrientationEventListener;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.view.Window;
 import android.widget.FrameLayout;
 
 import com.getcapacitor.JSObject;
@@ -51,10 +52,31 @@ public class SafeAreaPlugin extends Plugin {
         }
     }
 
+
     @PluginMethod
     public void setImmersiveNavigationBar(PluginCall call) {
-        bridge.getActivity().runOnUiThread(() -> bridge.getActivity().getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE));
+        Window window = bridge.getActivity().getWindow();
+        // Use a transparent status bar and nav bar, and place the window behind the status bar
+        // and nav bar. Due to a chromium bug, we need to get the height of both bars
+        // and add it to the safe area insets. The native plugin is used to get this info.
+        // See https://bugs.chromium.org/p/chromium/issues/detail?id=1094366
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false);
+            window.setStatusBarColor(0);
+            window.setNavigationBarColor(0);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            // On older versions of android setDecorFitsSystemWindows doesn't exist yet, but it can
+            // be emulated with flags.
+            // It still must be P or greater, as that is the min version for getting the insets
+            bridge.getActivity().runOnUiThread(() -> {
+                View decorView = window.getDecorView();
+                decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                );
+                window.setStatusBarColor(0);
+                window.setNavigationBarColor(0);
+            });
+        }
         call.resolve();
     }
 
